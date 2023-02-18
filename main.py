04b27e011976b39d762a51f69e7ca53cb919fcdb
Shalika001 <shalikaupamada@gmail.com>
2023-02-18 22:15:25 +0530

    Opencv DNN

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2
 
-cpa = cv2.VideoCapture(0)
+#opencv DNN
+net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
+model = cv2.dnn_DetectionModel()
+model.setInputParams(size=(320, 320), scale=1/255)
+
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
