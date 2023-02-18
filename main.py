c6dbfa405aae66169c4fad2e4125369857e59cbd
Shalika001 <shalikaupamada@gmail.com>
2023-02-18 22:18:17 +0530

    Comments

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cf
 model = cv2.dnn_DetectionModel()
 model.setInputParams(size=(320, 320), scale=1/255)
 
+#Initialize camara
 cap = cv2.VideoCapture(0)
 
 while True:
+    #Get frames
     ret, frame = cap.read()
 
     cv2.imshow("Frame", frame)
