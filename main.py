5a54e0f04370f836efcc3b6f587b967e3b9e953e
Shalika001 <shalikaupamada@gmail.com>
2023-02-20 17:51:57 +0530

    comments

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 
 #Opencv DNN
 net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
+model = cv2.dnn_DetectionModel()
+model.setInputParams(size =(320, 320), scale = 1/255)
 
 #Initialize camara
 cap = cv2.VideoCapture(0)
@@ -9,6 +11,9 @@ cap = cv2.VideoCapture(0)
 while True:
     #Get frames
     ret, frame = cap.read()
+
+    #Object detection
+
 
     cv2.imshow("Frame", frame)
     cv2.waitKey(1)
