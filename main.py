5c8673bac9e229170a212a5633b659822eec2508
Shalika001 <shalikaupamada@gmail.com>
2023-02-20 17:32:50 +0530

    Add dnn file

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import cv2
 
-#opencv DNN
+#Opencv DNN
 net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
-model = cv2.dnn_DetectionModel()
-model.setInputParams(size=(320, 320), scale=1/255)
 
 #Initialize camara
 cap = cv2.VideoCapture(0)
