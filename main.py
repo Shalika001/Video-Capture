5bcfd68fcd22ab3a1076e14ba7f547bccae0ee43
Shalika001 <shalikaupamada@gmail.com>
2023-02-20 17:59:59 +0530

    Object detection

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 
 #Opencv DNN
 net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
-model = cv2.dnn_DetectionModel()
+model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size =(320, 320), scale = 1/255)
 
 #Initialize camara
@@ -13,6 +13,10 @@ while True:
     ret, frame = cap.read()
 
     #Object detection
+    (class_ids, scores, bboxes) = model.detection(frame)
+    print("class_ids", class_ids)
+    print("scores", scores)
+    print("bboxes", bboxes)
 
 
     cv2.imshow("Frame", frame)
