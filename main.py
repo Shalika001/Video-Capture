335cd05850c1b599f789126ff43ca37129dafa1e
Shalika001 <shalikaupamada@gmail.com>
2023-02-21 21:04:02 +0530

    Load class lists

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ net = cv2.dnn.readNet("./dnn_model/yolov4-tiny.weights", "./dnn_model/yolov4-tin
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size=(320, 320), scale=1/255)
 
+#Load class lists
+classes = []
+with open("dnn_model/classes.txt", "r") as file_object:
+    for class_name in file_object.readlines():
+        print(class_name)
+
 #Initialize camara
 cap = cv2.VideoCapture(0)
 
@@ -17,7 +23,7 @@ while True:
     for class_ids, score, bbox in zip(class_ids, scores, bboxes):
         (x, y, w, h) =bbox
 
-        cv2.putText(frame, str(class_ids), (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 1,(200, 0, 50), 2)
+        cv2.putText(frame, str(class_ids), (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 2, (200, 0, 50), 2)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (200, 0, 50), 3)
 
     print("class_ids", class_ids)
