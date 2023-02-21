3afa0a6c7545f4223a77ff4ae5f106d9f98a9235
Shalika001 <shalikaupamada@gmail.com>
2023-02-21 15:49:56 +0530

    Object detection

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 
 #Opencv DNN
-net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
+net = cv2.dnn.readNet("./dnn_model/yolov4-tiny.weights", "./dnn_model/yolov4-tiny.cfg")
 model = cv2.dnn_DetectionModel(net)
-model.setInputParams(size =(320, 320), scale = 1/255)
+model.setInputParams(size=(320, 320), scale=1/255)
 
 #Initialize camara
 cap = cv2.VideoCapture(0)
@@ -13,7 +13,7 @@ while True:
     ret, frame = cap.read()
 
     #Object detection
-    (class_ids, scores, bboxes) = model.detection(frame)
+    (class_ids, scores, bboxes) = model.detect(frame)
     print("class_ids", class_ids)
     print("scores", scores)
     print("bboxes", bboxes)
